cce3fc20c052959168cc8a77c1d52fb44c89e006
ItzThatKidBrian <sampsonbrian863@gmail.com>
2022-08-02 21:49:43 -0400

    Added ejs to make all the buttons work
    Buttons are now able to redirect to other pages.
    
    Changed port from 8080 to 3000.
    Fixed some syntax  errors in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 
 dotenv.config({path:'config.env'})
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 //Log request
 app.use(morgan('tiny'));
@@ -20,14 +20,22 @@ app.set("view engine", "ejs")  //TimeStamp: 19:24 (set our view engine to use ej
 //app.set("views", path.resolve(__dirname, "views/ejs")) //20:30 "just for reference"
 
 //loading assests
-app.use('/css',express.static(path.resolve(__dirname, "assests/css")))  //22:00
-app.use('/img',express.static(path.resolve(__dirname, "assests/img")))
-app.use('/js',express.static(path.resolve(__dirname, "assests/js")))
+app.use('/css',express.static(path.resolve(__dirname, "assets/css")))  //22:00
+app.use('/img',express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js',express.static(path.resolve(__dirname, "assets/js")))
 
 
 app.get('/', (req,res)=> {
   //Render allows us to render a html file (ejs = embeded JS)
   res.render('index.ejs');
+})
+
+app.get('/add-user', (req,res)=> {
+  res.render('add_user');
+})
+
+app.get('/update-user', (req,res)=> {
+  res.render('update_user');
 })
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
